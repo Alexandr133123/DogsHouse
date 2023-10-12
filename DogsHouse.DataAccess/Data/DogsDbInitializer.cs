a85156d5f66417ea35c654650e6bfa0ba0255f44
Alexandr <gagahre4@gmail.com>
2023-10-12 15:22:53 +0300

    Fixed DA layer DI class (added scoped IDogsDbContext for Application). Modified Initializer class (added SeedMethod)

--- DogsHouse.DataAccess/Data/DogsDbInitializer.cs
+++ DogsHouse.DataAccess/Data/DogsDbInitializer.cs
@@ -1,16 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DogsHouseService.Domain;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DogsHouse.DataAccess.Data
 {
-    public class DogsDbInitializer
+    public static class DogsDbInitializer
     {
-        public static void Initialize(DogsDbContext dbContext)
+        public static async Task InitializeDbAsync(this WebApplication app)
         {
+            var dbContext = app.Services
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<DogsDbContext>();            
+            
             dbContext.Database.EnsureCreated();
+
+            if (!dbContext.Dogs.Any())
+            {
+                await Seed(dbContext);
+            }
+        }
+
+        private static async Task Seed(DogsDbContext dbContext)
+        {
+            dbContext.Dogs.AddRange(
+                    new Dog
+                    {
+                        Name = "Rex",
+                        Color = "Brown",
+                        TailLength = 20,
+                        Weight = 30,
+                    },
+                    new Dog
+                    {
+                        Name = "Fido",
+                        Color = "Black",
+                        TailLength = 15,
+                        Weight = 25,
+                    },
+                    new Dog
+                    {
+                        Name = "Bella",
+                        Color = "White",
+                        TailLength = 25,
+                        Weight = 22,
+                    },
+                    new Dog
+                    {
+                        Name = "Rocky",
+                        Color = "Gray",
+                        TailLength = 18,
+                        Weight = 35,
+                    },
+                    new Dog
+                    {
+                        Name = "Luna",
+                        Color = "Golden",
+                        TailLength = 22,
+                        Weight = 28,
+                    },
+                    new Dog
+                    {
+                        Name = "Charlie",
+                        Color = "Spotted",
+                        TailLength = 17,
+                        Weight = 31,
+                    },
+                    new Dog
+                    {
+                        Name = "Daisy",
+                        Color = "Cream",
+                        TailLength = 19,
+                        Weight = 29,
+                    },
+                    new Dog
+                    {
+                        Name = "Buddy",
+                        Color = "Red",
+                        TailLength = 21,
+                        Weight = 27,
+                    },
+                    new Dog
+                    {
+                        Name = "Milo",
+                        Color = "Brindle",
+                        TailLength = 16,
+                        Weight = 33,
+                    },
+                    new Dog
+                    {
+                        Name = "Max",
+                        Color = "Sable",
+                        TailLength = 23,
+                        Weight = 26,
+                    }
+                );
+
+            await dbContext.SaveChangesAsync();
         }
     }
 }
