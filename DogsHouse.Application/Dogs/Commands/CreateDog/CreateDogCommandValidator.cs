469a08fed80b560ab7efbd7245d251c1d0b1694b
Alexandr <gagahre4@gmail.com>
2023-10-12 18:08:23 +0300

    Added Fluent Validation to the API

---
+++ DogsHouse.Application/Dogs/Commands/CreateDog/CreateDogCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace DogsHouse.Application.Dogs.Commands.CreateDog
+{
+    public class CreateDogCommandValidator : AbstractValidator<CreateDogCommand>
+    {
+        public CreateDogCommandValidator() 
+        {
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .WithMessage("Name can't be empty");
+
+            RuleFor(d => d.Color)
+                .NotEmpty()
+                .WithMessage("Color can't be empty");
+
+            RuleFor(d => d.Weight)
+                .NotEmpty()
+                .WithMessage("Weight can't be empty")
+                .Must(w => w > 0)
+                .WithMessage($"Weight can't less/equal 0");
+
+            RuleFor(d => d.TailLength)
+                .NotEmpty()
+                .WithMessage("Tail length can't be empty")
+                .Must(tl => tl > 0)
+                .WithMessage($"Tail can't be less/equal 0");
+            
+        }
+    }
+}
---
+++ DogsHouse.Application/Dogs/Commands/UpdateDog/UpdateDogCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace DogsHouse.Application.Dogs.Commands.UpdateDog
+{
+    public class UpdateDogCommandValidator : AbstractValidator<UpdateDogCommand>
+    {
+        public UpdateDogCommandValidator() 
+        {
+            RuleFor(d => d.Id).NotEmpty();
+            RuleFor(d => d.Name).NotEmpty();
+            RuleFor(d => d.Color).NotEmpty();
+            RuleFor(d => d.Weight).NotEmpty().Must(w => w > 0);
+            RuleFor(d => d.TailLength).NotEmpty().Must(tl => tl > 0);
+        }
+    }
+}
